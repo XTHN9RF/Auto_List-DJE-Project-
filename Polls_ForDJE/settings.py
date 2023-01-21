cb2cd5174e41fd873afe85d21327dbcecea193ce
xthn9rf <justez4rtz@gmail.com>
2023-01-21 21:51:31 +0200

    fix/moved polls dir from polls_fordje dir

--- Polls_ForDJE/settings.py
+++ Polls_ForDJE/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from Polls_ForDJE.polls.apps import PollsConfig
+import polls
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'polls.apps.PollsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
