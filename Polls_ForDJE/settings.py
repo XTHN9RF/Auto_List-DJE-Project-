300de5adaf30899e3948f22fb646612cbf73e4c8
xthn9rf <justez4rtz@gmail.com>
2023-02-28 16:38:07 +0200

    fix:removed home app from installed apps

--- Polls_ForDJE/settings.py
+++ Polls_ForDJE/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello.apps.HelloConfig',
-    'home.apps.HomeConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
