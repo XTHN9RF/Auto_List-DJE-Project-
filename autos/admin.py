422cc841a2589669361fdf2849fedbd56750a194
Duran Vladyslav <justez4rtz@gmail.com>
2023-03-08 15:45:47 +0200

    fix: fixed incorrect import of models to admin

--- autos/admin.py
+++ autos/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from models import Make, Auto
+from autos.models import Make, Auto
 
 admin.register(Make)
 admin.register(Auto)
--- cats/admin.py
+++ cats/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from models import Cat, Breed
+from cats.models import Cat, Breed
 
 admin.register(Cat)
 admin.register(Breed)
