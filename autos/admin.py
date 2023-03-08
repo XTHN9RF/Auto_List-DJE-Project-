7d48e4291c701f9a334831660d263e7818ba6a2c
Duran Vladyslav <justez4rtz@gmail.com>
2023-03-08 15:17:19 +0200

    feat: registered models to admin for autos and cats apps

--- autos/admin.py
+++ autos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Make, Auto
+
+admin.register(Make)
+admin.register(Auto)
--- cats/admin.py
+++ cats/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Cat, Breed
+
+admin.register(Cat)
+admin.register(Breed)
